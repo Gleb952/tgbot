ec1988bd4d785320d777e7de654ddb71d7f7b97d
IT Студент <it_student@Kvant78.super>
2023-10-01 12:40:20 +0300

    ready to coding

---
+++ main.py
@@ -0,0 +1,16 @@
+import configparser
+import mysql.connector
+import telebot
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+bot = telebot.TeleBot(config['BOT']['TOKEN'])
+
+con = mysql.connector.connect(host='192.168.0.2', user='root', password='194@d&bF@&f$bd#s')
+cursor = con.cursor()
+cursor.execute('CREATE DATABASE reqests')
+print(cursor)
+
